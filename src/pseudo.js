604dbda125892db2f6162bccb110e7a2e63ee088
Sabrina Hanson <sabhanson7@gmail.com>
2022-03-17 22:45:06 -0700

    psuedocode

---
+++ src/pseudo.js
@@ -0,0 +1,39 @@
+// AS A hotel guest
+// I WANT to see a visual representation of all amenities available to me
+// SO THAT I can make the most use out of my time and enjoy all benefits of the hotel/resort
+
+// admin capabilities
+// CRUD functionality database tables and contents
+// TABLES: resort departments, items for sale, appointments
+// text into the database for easy updates
+
+// user capabilities
+// make an account on the app
+// subscribe to notifications
+// text a number to ask questions easily
+// make appointments
+// look at restaurant's offerings
+
+// ROUTES:
+// sign up
+// login
+// make new department
+// list an item for sale
+// receive an order from a customer (only offer pay in cash rn haha)
+// make a appointment
+
+// navbar elements/different pages
+// dashboard
+// reservation
+// spa + fitness
+// restaurant
+// happenings
+
+// card component with 2x2 grid that will be rendered on each page with different content
+// 4 boxes in each card to render different text/buttons/links
+
+// keep app really simple as far as content on page. more pleasing to eye, easier to navigate
+
+// mobile formatting, navbar will be sticky item in top corner, when pressed dropdown menu appears
+// tablet formatting, navbar will be stationary on left screen
+// desktop formatting, navbar will be stationary on left screen
